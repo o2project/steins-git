557cac2d4a87ea46cf7096df15e4d12d4f697ce1
kubosho <ta2@o2p.jp>
2015-03-03 10:34:28 +0900

    chore(analytics): O2 ProjectのGitHub Pagesのドメインにした

--- public/google-analytics.js
+++ public/google-analytics.js
@@ -3,7 +3,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-25380313-6', 'kubosho.github.io');
+ga('create', 'UA-25380313-6', 'o2project.github.io');
 ga('send', 'pageview');
 
 window.addEventListener("hashchange", function hashChange() {
