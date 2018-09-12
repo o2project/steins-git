795d73d7484c20b4279b55fb231345ec87813ea8
Shota Kubota <ta2@o2p.jp>
2018-09-13 00:02:14 +0900

    feat(pages): add introduction page

---
+++ src/pages/introduction.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-html="mdContents"></div>
+</template>
+
+<script>
+export default {
+  async asyncData() {
+    const mdContents = await import(`../drafts/introduction.md`);
+
+    return {
+      mdContents,
+    }
+  }
+}
+</script>
