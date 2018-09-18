41af72d543edf0e805f7070b2b7feea5e2e1b331
Shota Kubota <ta2@o2p.jp>
2018-09-18 22:37:22 +0900

    feat(config): use foundation style files

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ module.exports = {
   srcDir: 'src/',
   css: [
     '@/assets/styles/reset.css',
+    '@/assets/styles/vars.css',
+    '@/assets/styles/foundation.css',
   ],
   modules: [
     '@nuxtjs/markdownit'
