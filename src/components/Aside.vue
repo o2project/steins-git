864ece25a3a38b2b619e6b452a4b98715b10a346
Shota Kubota <ta2@o2p.jp>
2018-09-18 11:44:32 +0900

    feat(components): add Aside components

---
+++ src/components/Aside.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <template v-for="(group, index) in menu">
+      <h3 :key="index">{{ group.title }}</h3>
+      <ul :key="index">
+        <li v-for="(content, idx) in group.contents" :key="index">
+          <nuxt-link :to="content.link" :key="idx" exact>
+            {{ content.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </template>
+  </nav>
+</template>
+
+<script>
+export default {
+  computed: {
+    menu() { return this.$store.state.menu },
+  },
+}
+</script>
