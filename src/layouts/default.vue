b7965ac5c69c048faf7177441eac3b59d1443ce6
Shota Kubota <ta2@o2p.jp>
2018-09-18 12:03:25 +0900

    feat(layouts): add default layout file

---
+++ src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt />
+    <sg-aside />
+  </div>
+</template>
+
+<script>
+import SgAside from '~/components/Aside.vue';
+
+export default {
+  components: {
+    SgAside,
+  },
+}
+</script>
