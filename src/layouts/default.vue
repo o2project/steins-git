c5d16ea3d29f7cec5c0e0861b08c26126177d0e6
Shota Kubota <ta2@o2p.jp>
2018-09-21 01:24:37 +0900

    feat(layouts): change default layout style

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="container">
-    <header class="header">
-      <h1 class="title">Steins;Git <span class="sub">Rebuild</span></h1>
-    </header>
     <main class="contents">
       <nuxt class="chapter" />
       <sg-toc class="toc" />
     </main>
     <footer class="footer">
+      <p class="site-title">Steins;Git <span class="sub">Rebuild</span></p>
       <p class="copyrights">
         <small>© 2014 O2 Project.</small>
       </p>
@@ -26,33 +24,68 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.title {
-  margin: 0;
-  font-size: 1.5em;
-}
+@import "../assets/styles/vars.css";
 
-.title .sub {
-  text-transform: uppercase;
+.container {
+}
+@media screen and (min-width: 64em) {
+  .container {
+    position: relative;
+    min-height: 100vh;
+  }
 }
 
 .contents {
-  display: grid;
-  grid-template-columns: 20em 1fr;
+}
+@media screen and (min-width: 64em) {
+  .contents {
+    display: grid;
+    grid-template-columns: 20em 1fr;
+
+    .chapter,
+    .toc {
+      max-height: 100vh;
+      padding: 0 0.5em;
+      overflow-y: auto;
+    }
+
+    .chapter {
+      grid-column: 2;
+    }
+
+    .toc {
+      grid-column: 1;
+      grid-row: 1;
+    }
+  }
 }
 
-.chapter {
-  grid-row: 1;
-  grid-column: 2;
-  max-height: 100vh;
-  padding: 0 0.5em;
-  overflow-y: auto;
+.footer {
+}
+@media screen and (min-width: 64em) {
+  .footer {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    bottom: 0;
+  }
 }
 
-.aside {
-  grid-row: 1;
-  grid-column: 1;
-  max-height: 100vh;
-  padding: 0 0.5em;
-  overflow-y: auto;
+.site-title {
+  margin: 0;
+  font-weight: bold;
+
+  .sub {
+    text-transform: uppercase;
+  }
+}
+@media screen and (min-width: 64em) {
+  .site-title {
+    margin: 0 0.5em 0;
+  }
+}
+
+.copyrights {
+  margin: 0;
 }
 </style>
